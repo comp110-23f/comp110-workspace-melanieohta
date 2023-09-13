90b35c511bbd0e46c61dbee7abcbe7d9c40ed758
Melanie Ohta <mohta@unc.edu>
2023-09-13 11:30:27 -0400

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,39 @@
+"""Ex01 - Chardle, a freshman CS major's version of wordle."""
+
+__author__ = "730671130"
+
+word: str = input("Enter a 5-character word: ")
+if(len(word) != 5):
+    print("Error: Word must contain 5 characters")
+    exit()
+
+character: str = input("Enter a single character: ")
+if(len(character) != 1):
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + character + " in " + word)
+
+num_match: int = 0
+
+if(word[0] == character):
+    print(character + " found at index 0")
+    num_match += 1
+
+if(word[1] == character):
+    print(character + " found at index 1")
+    num_match += 1
+
+if(word[2] == character):
+    print(character + " found at index 2")
+    num_match += 1
+
+if(word[3] == character):
+    print(character + " found at index 3")
+    num_match += 1
+
+if(word[4] == character):
+    print(character + " found at index 4")
+    num_match += 1
+
+print(str(num_match) + " instances of " + character + " found in " + word)
