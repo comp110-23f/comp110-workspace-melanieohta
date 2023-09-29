65929be4dcd91c88c59b0ae51ee4882f79cdd38e
Melanie Ohta <mohta@unc.edu>
2023-09-29 09:53:31 -0400

    Progress on Exercise 3

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,9 +2,10 @@
 
 __author__ = "730671130"
 
-"""This function searches through a word 'guess' for a specific character 'char'. If 
-the word contains the character, it returns true. If not, false."""
+
 def contains_char(word: str, char: str) -> bool:
+    """This function searches through a word 'guess' for a specific character 'char'. 
+    If the word contains the character, it returns true. If not, false."""
     assert len(char) == 1
     index: int = 0
     while index < len(word):
@@ -13,14 +14,16 @@ def contains_char(word: str, char: str) -> bool:
         index += 1
     return False
 
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-"""This function takes a guess and a secret word and returns a green emoji for a 
-letter in the right place, yellow for a letter in the wrong spot, and white for a
-letter not in the secret word."""
+
 def emojified(user_guess: str, secret_word: str) -> str:
+    """This function takes a guess and a secret word and returns a green emoji for a 
+    letter in the right place, yellow for a letter in the wrong spot, and white for a
+    letter not in the secret word."""
     assert len(user_guess) == len(secret_word)
     idx: int = 0
     emoji: str = ""
@@ -35,29 +38,32 @@ def emojified(user_guess: str, secret_word: str) -> str:
         idx += 1
     return emoji
 
-"""This function asks the user to enter a word of a certain number of characters."""
+
 def input_guess(num_char: int) -> str:
+    """This function asks the user to enter a word of a certain number of characters."""
     guess: str = input(f"Enter a { num_char } character word: ")
     while len(guess) != num_char:
-        guess: str = input(f"That wasn't { num_char } chars! Try again: ")
+        guess = input(f"That wasn't { num_char } chars! Try again: ")
     return guess
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
     turn: int = 1
     game_won: bool = False
     secret: str = "max"
-    while turn <= 6 and game_won == False:
+    while turn <= 6 and game_won is False:
         print(f"=== Turn { turn }/6 ===")
         new_guess: str = input_guess(len(secret))
         print(emojified(new_guess, secret))
         if new_guess == secret:
             game_won = True
         turn += 1
-    if game_won == True:
-       print(f"You won in { turn }/6 turns!")
+    if game_won is True:
+        print(f"You won in { turn }/6 turns!")
     else:
         print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
