8ad28f596e12f0c91f886eafd37876e37fa19293
Melanie Ohta <mohta@unc.edu>
2023-09-30 02:35:40 -0400

    Progress on Ex 3

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,8 +4,7 @@ __author__ = "730671130"
 
 
 def contains_char(word: str, char: str) -> bool:
-    """This function searches through a word 'guess' for a specific character 'char'. 
-    If the word contains the character, it returns true. If not, false."""
+    """This function searches through a word 'guess' for a specific character 'char'."""
     assert len(char) == 1
     index: int = 0
     while index < len(word):
@@ -21,9 +20,7 @@ YELLOW_BOX: str = "\U0001F7E8"
 
 
 def emojified(user_guess: str, secret_word: str) -> str:
-    """This function takes a guess and a secret word and returns a green emoji for a 
-    letter in the right place, yellow for a letter in the wrong spot, and white for a
-    letter not in the secret word."""
+    """This function compares a guess and a secret word to make an emojified version of wordle."""
     assert len(user_guess) == len(secret_word)
     idx: int = 0
     emoji: str = ""
@@ -42,6 +39,7 @@ def emojified(user_guess: str, secret_word: str) -> str:
 def input_guess(num_char: int) -> str:
     """This function asks the user to enter a word of a certain number of characters."""
     guess: str = input(f"Enter a { num_char } character word: ")
+    # continues to ask player to input a guess until they get the correct length
     while len(guess) != num_char:
         guess = input(f"That wasn't { num_char } chars! Try again: ")
     return guess
@@ -52,16 +50,16 @@ def main() -> None:
     turn: int = 1
     game_won: bool = False
     secret: str = "max"
+    # continues until player reaches 6 turns or they win the game
     while turn <= 6 and game_won is False:
         print(f"=== Turn { turn }/6 ===")
         new_guess: str = input_guess(len(secret))
         print(emojified(new_guess, secret))
         if new_guess == secret:
             game_won = True
+            print(f"You won in { turn }/6 turns!")
         turn += 1
-    if game_won is True:
-        print(f"You won in { turn }/6 turns!")
-    else:
+    if game_won is False:
         print("X/6 - Sorry, try again tomorrow!")
 
 
