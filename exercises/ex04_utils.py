086dc1306b4abe9a9d7b5750cef67b63077f3105
Melanie Ohta <mohta@unc.edu>
2023-10-11 10:45:24 -0400

    Progress on ex4

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -10,6 +10,7 @@ def all(num_list: list[int], num: int) -> bool:
     while i < len(num_list):
         if num_list[i] == num:
             count += 1
+        i += 1
     if count == len(num_list):
         return True
     return False
@@ -24,15 +25,20 @@ def max(input: list[int]) -> int:
     while i < len(input):
         if input[i] > max_int:
             max_int = input[i]
+        i += 1
     return max_int
 
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
     """Returns True if both given lists are deeply equal/exactly the same."""
-    if(len(list1) != len(list2)):
+    if len(list1) != len(list2):
         return False
     i: int = 0
     while i < len(list1):
         if list1[i] != list2[i]:
             return False
     return True
+
+print(all([1, 2, 3], 1))
+print(max([1, 3, 2]))
+print(is_equal([1, 0, 1], [1, 0, 1]))
