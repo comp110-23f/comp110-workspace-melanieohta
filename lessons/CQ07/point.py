642766497c0a9ace396812c48a7c5b9b179af737
Melanie Ohta <mohta@unc.edu>
2023-11-09 11:55:19 -0500

    CQ07 Update

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -11,7 +11,7 @@ class Point:
     x: float
     y: float
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """Constructor."""
         self.x = x_init
         self.y = y_init
@@ -24,4 +24,19 @@ class Point:
     def scale(self, factor: int) -> Point:
         """Returns a new point with x and y scaled by the factor."""
         new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+    def __str__(self) -> str:
+        """String magic method that prints points in readable way."""
+        p_string: str = f"x: {self.x}; y: {self.y}"
+        return p_string
+    
+    def __mul__(self, factor: int | float) -> Point:
+        """Multiplication magic method that overloads the * operator."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+    def __add__(self, num: int | float) -> Point:
+        """Addition magic method that overloads the + operator."""
+        new_point: Point = Point(self.x + num, self.y + num)
         return new_point
